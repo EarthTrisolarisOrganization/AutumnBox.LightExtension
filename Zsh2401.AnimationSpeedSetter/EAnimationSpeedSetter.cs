304d41317fd563759cc58058bf1715b26a328611
zsh2401 <zsh2401@163.com>
2018-12-18 03:16:11 +0800

    完成了几个模块,动画设置器似乎由于安卓版本问题无法实现

---
+++ Zsh2401.AnimationSpeedSetter/EAnimationSpeedSetter.cs
@@ -0,0 +1,25 @@
+﻿using AutumnBox.OpenFramework.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zsh2401.AnimationSpeedSetter
+{
+    [ExtName("动画速度调节器")]
+    [ExtDesc("其实与开发者选项里能调的没啥区别")]
+    [ExtAuth("zsh2401")]
+    [ExtIcon("animation.png")]
+    [ExtRequiredDeviceStates(AutumnBox.Basic.Device.DeviceState.Poweron)]
+    class EAnimationSpeedSetter : SharpExtension
+    {
+        protected override void Processing(Dictionary<string, object> data)
+        {
+            RunOnUIThread(() =>
+            {
+                new MainWindow(this).ShowDialog();
+            });
+        }
+    }
+}
---
+++ Zsh2401.AnimationSpeedSetter/VMMain.cs
@@ -0,0 +1,144 @@
+﻿using AutumnBox.Basic.Calling;
+using AutumnBox.Basic.Device;
+using AutumnBox.OpenFramework.Content;
+using AutumnBox.OpenFramework.Open;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zsh2401.MVVM;
+
+namespace Zsh2401.AnimationSpeedSetter
+{
+    class VMMain : ViewModelBase
+    {
+        public IDevice Device
+        {
+            get => _device; set
+            {
+                _device = value;
+                RaisePropertyChanged();
+            }
+        }
+        private IDevice _device;
+
+        public Context Context
+        {
+            get => _ctx; set
+            {
+                _ctx = value;
+                if (Device == null)
+                {
+                    Device = _ctx.GetService<IDeviceSelector>(ServicesNames.DEVICE_SELECTOR).GetCurrent(_ctx);
+                }
+            }
+        }
+        private Context _ctx;
+
+        public string WindowSpeed
+        {
+            get => _windowSpeed; set
+            {
+                _windowSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _windowSpeed = "0.75";
+
+        public string AnimatorSpeed
+        {
+            get => _animatorSpeed; set
+            {
+                _animatorSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _animatorSpeed = "0.75";
+
+        public string TransitionSpeed
+        {
+            get => _transitionSpeed; set
+            {
+                _transitionSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _transitionSpeed = "0.75";
+
+        public FlexiableCommand SetWindowSpeed
+        {
+            get => _setWindowSpeed;
+            set
+            {
+                _setWindowSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private FlexiableCommand _setWindowSpeed;
+
+        public FlexiableCommand SetAnimatorSpeed
+        {
+            get => _setAnimatorSpeed;
+            set
+            {
+                _setAnimatorSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private FlexiableCommand _setAnimatorSpeed;
+
+        public FlexiableCommand SetTransitionSpeed
+        {
+            get => _setTransitionSpeed;
+            set
+            {
+                _setTransitionSpeed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private FlexiableCommand _setTransitionSpeed;
+
+        private readonly CommandExecutor executor;
+        public VMMain()
+        {
+            executor = new CommandExecutor();
+            SetWindowSpeed = new FlexiableCommand(SetWindowSpeedAction);
+            SetAnimatorSpeed = new FlexiableCommand(SetAnimatorSpeedAction);
+            SetTransitionSpeed = new FlexiableCommand(SetTransitionSpeedAction);
+        }
+        private void SetWindowSpeedAction()
+        {
+            if (float.TryParse(WindowSpeed, out float num))
+            {
+                executor.AdbShell(Device, "settings put global window_animation_scale", num.ToString());
+            }
+            else
+            {
+                Context.Ux.Warn("请输入一个正确的数字!");
+            }
+        }
+        private void SetAnimatorSpeedAction()
+        {
+            if (float.TryParse(AnimatorSpeed, out float num))
+            {
+                executor.AdbShell(Device, "settings put global transition_animation_scale ", num.ToString());
+            }
+            else
+            {
+                Context.Ux.Warn("请输入一个正确的数字!");
+            }
+        }
+        private void SetTransitionSpeedAction()
+        {
+            if (int.TryParse(TransitionSpeed, out int num))
+            {
+                executor.AdbShell(Device, "settings put global animator_duration_scale ", num.ToString());
+            }
+            else
+            {
+                Context.Ux.Warn("请输入一个正确的数字!");
+            }
+        }
+    }
+}
