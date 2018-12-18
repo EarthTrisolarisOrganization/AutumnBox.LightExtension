48b391f38b741b20dd34a517d79b45a986ccf860
zsh2401 <zsh2401@163.com>
2018-12-18 22:11:41 +0800

    修改版本号

--- Zsh2401.AnimationSpeedSetter/EAnimationSpeedSetter.cs
+++ Zsh2401.AnimationSpeedSetter/EAnimationSpeedSetter.cs
@@ -8,9 +8,10 @@ using System.Threading.Tasks;
 namespace Zsh2401.AnimationSpeedSetter
 {
     [ExtName("动画速度调节器")]
-    [ExtDesc("其实与开发者选项里能调的没啥区别")]
+    [ExtDesc("其实与开发者选项里能调的没啥区别,并且在部分安卓系统中,此模块的实现方法已经失效")]
     [ExtAuth("zsh2401")]
     [ExtIcon("animation.png")]
+    [ExtVersion(0,0,5)]
     [ExtRequiredDeviceStates(AutumnBox.Basic.Device.DeviceState.Poweron)]
     class EAnimationSpeedSetter : SharpExtension
     {
